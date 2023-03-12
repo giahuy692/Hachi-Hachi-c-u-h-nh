89d2b93c038dfd2159dbc466045d6919018a2a9b
Do Gia Huy <it.dogiahuy@gmail.com>
2023-03-12 12:25:52 +0700

    Sửa lại active của pagination

--- src/app/pagination/pagination.component.ts
+++ src/app/pagination/pagination.component.ts
@@ -1,16 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-pagination',
   templateUrl: './pagination.component.html',
   styleUrls: ['./pagination.component.scss'],
 })
-export class PaginationComponent {
+export class PaginationComponent implements OnInit {
+  ngOnInit() {}
   selected = 'option2';
 
-  isActive: boolean = false;
+  currentPage: number = 1;
 
-  buttonActive() {
-    this.isActive = true;
+  pages = Array.from({ length: 4 }, (_, i) => i + 1);
+
+  setCurrentPage(page: number) {
+    this.currentPage = page;
   }
 }
