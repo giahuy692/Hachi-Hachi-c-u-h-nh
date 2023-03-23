f6f4f0ba41405ae618d3641ff3e34dc51c24ff11
giahuy692 <yuhaigod8485@gmail.com>
2023-03-23 09:45:58 +0700

    Policy - UI - Cắt giao diện - cắt giao diện sidebar và checkbox

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -17,10 +17,12 @@ import { CategoryService } from './service/category/category.service';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 //kendon
-import { LabelModule } from '@progress/kendo-angular-label'; //11:39 - 17/3
-import { InputsModule } from '@progress/kendo-angular-inputs'; //11:35 - 17/3
-import { DropDownsModule } from '@progress/kendo-angular-dropdowns'; //3:35 - 17/3
-import { GridModule } from '@progress/kendo-angular-grid'; //3:50 - 17/3
+import { LabelModule } from '@progress/kendo-angular-label';
+import { InputsModule } from '@progress/kendo-angular-inputs';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { GridModule } from '@progress/kendo-angular-grid';
+import { TreeListModule } from '@progress/kendo-angular-treelist';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
 
 //Componenet
 import { AppRoutingModule } from './app-routing.module';
@@ -31,7 +33,6 @@ import { CheckboxComponent } from './checkbox/checkbox.component';
 import { FilterGroupComponent } from './filter-group/filter-group.component';
 import { SelectDataComponent } from './select-data/select-data.component';
 import { TableProductComponent } from './table-product/table-product.component';
-import { PaginationComponent } from './pagination/pagination.component';
 
 @NgModule({
   declarations: [
@@ -42,7 +43,6 @@ import { PaginationComponent } from './pagination/pagination.component';
     FilterGroupComponent,
     SelectDataComponent,
     TableProductComponent,
-    PaginationComponent,
   ],
   imports: [
     BrowserModule,
@@ -58,6 +58,8 @@ import { PaginationComponent } from './pagination/pagination.component';
     LabelModule,
     DropDownsModule,
     GridModule,
+    TreeListModule,
+    ButtonsModule,
   ],
   providers: [ProductService, CategoryService],
   bootstrap: [AppComponent],
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
+
+// Jquery
+import * as $ from 'jquery';
+
+class navbars {
+  title: string = '';
+  subtitle?: any[];
+}
 
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
-  styleUrls: ['./sidebar.component.scss']
+  styleUrls: ['./sidebar.component.scss'],
 })
-export class SidebarComponent {
+export class SidebarComponent implements AfterViewInit {
+  navbars: navbars[] = [
+    { title: 'KHUYẾN MÃI', subtitle: undefined },
+    { title: 'NỘI DUNG WEBSITE', subtitle: undefined },
+    { title: 'QUẢN LÝ BANNER', subtitle: undefined },
+    {
+      title: 'CHÍNH SÁCH',
+      subtitle: [
+        'Coupon',
+        'xxxxxxxxxxxxx',
+        'xxxxxxxxxxxxx',
+        'xxxxxxxxxxxxx',
+        'xxxxxxxxxxxxx',
+        'xxxxxxxxxxxxx',
+      ],
+    },
+    { title: 'BÁO CÁO EXCEL', subtitle: undefined },
+  ];
 
+  onClickSub(value: navbars) {
+    if (value.subtitle != undefined) {
+      $(document).ready(function () {
+        $('.subContainer').off().addClass('showSub');
+      });
+    } else {
+      $(document).ready(function () {
+        $('.subContainer').off().removeClass('showSub');
+      });
+    }
+  }
+
+  ngAfterViewInit() {
+    $(document).ready(() => {
+      $('.submenu').removeClass('k-rounded-md');
+      $('.btn_link')
+        .off()
+        .on('click', function () {
+          $('.btn_link').removeClass('h-btn-active');
+          $('.btn_link').removeClass('k-rounded-md');
+          $(this).addClass('h-btn-active');
+        });
+    });
+  }
 }
